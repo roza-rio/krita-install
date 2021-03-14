259ced86cf23d643666de831bf0cbf29a4ee8ccc
Daniel Rozario <61970262+roza-rio@users.noreply.github.com>
2021-03-14 01:54:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo mv krita /bin/
 mkdir cd $HOME/.local/share/applications
 cd $HOME/.local/share/applications
 wget https://img.icons8.com/dusk/452/krita.png
-wget https://raw.githubusercontent.com/roza-rio/krita-install/main/krita.desktop
+wget https://raw.githubusercontent.com/roza-rio/krita-install/master/krita.desktop
 echo "Done!"
